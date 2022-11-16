b259a34608b619f2d7c1f948e7a311018f8a978d
João Pedro R. Norton <jaonorton@gmail.com>
2022-11-16 17:42:19 -0300

    Criado o programa

---
+++ ConsultaCatalogoStreaming/Services/MovieServices.cs
@@ -0,0 +1,56 @@
+﻿using ConsultaCatalogoStreaming.Models;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsultaCatalogoStreaming.Services
+{
+    public class MovieServices
+    {
+        private List<string> _streamings = new List<string>();
+        public MovieServices()
+        {
+            _streamings.Add("netflix");
+            _streamings.Add("prime"); 
+            _streamings.Add("disney"); 
+            _streamings.Add("hbo"); 
+            _streamings.Add("hulu"); 
+            _streamings.Add("peacock"); 
+            _streamings.Add("paramount"); 
+            _streamings.Add("starz"); 
+            _streamings.Add("showtime"); 
+            _streamings.Add("apple");  
+            _streamings.Add("mubi"); 
+        }
+
+        public async Task<MovieResponse> SerchForAMovieAsync(string movieName)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "1dd5235fc0msh5f27ce0820fed68p10fec2jsnf61fe5e443d1");
+            client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "streaming-availability.p.rapidapi.com");
+
+            MovieResponse movieResponse = new MovieResponse();
+
+            foreach (var item in _streamings)
+            {
+                string resposta = await client.GetStringAsync("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" + item  +  "&type=movie&keyword=El%20Camino%20&page=1&output_language=en&language=en");
+                Result filme = JsonConvert.DeserializeObject<Result>(resposta);
+
+                if (filme != null)
+                {
+                    //adicionar o link 
+                }
+               // movieResponse.TemNaNetflix = 
+            } //nome do filme
+            return movieResponse;
+           
+        }
+
+
+    }
+}
